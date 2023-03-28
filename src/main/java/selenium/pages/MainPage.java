cf5e3b329f47b27822d2744a70b79f9943631f26
khanusek <hanusek.karolina@gmail.com>
2023-03-28 19:57:41 +0200

    initial commit

---
+++ src/main/java/selenium/pages/MainPage.java
@@ -0,0 +1,10 @@
+package selenium.pages;
+
+import org.openqa.selenium.WebDriver;
+import selenium.BasePage;
+
+public class MainPage extends BasePage {
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+}
--- src/test/java/selenium/FirstTest.java
+++ src/test/java/selenium/FirstTest.java
@@ -2,11 +2,9 @@ package selenium;
 
 import org.junit.jupiter.api.Test;
 
-public class FirstTest extends BaseTest{
-
+public class FirstTest extends BaseTest {
 
     @Test
-    public void openGooglePage(){
-
+    public void openGooglePage() {
     }
 }
